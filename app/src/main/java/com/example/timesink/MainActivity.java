19898833668fdef25fa2a36269cca819f7af16c2
Jack Koscheka <koschekajack836@gmail.com>
2020-02-28 21:42:06 -0800

    added initial button and timer

---
+++ app/src/main/java/com/example/timesink/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.timesink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    final float FRAMES_PER_SECOND = 100; // MAX 1000 (INCLUSIVE), MIN 0 (EXCLUSIVE) FRAMES_PER_SECOND // 0 < FRAMES_PER_SECOND <= 1000
+
+    // ADD ALL OBJECTS HERE
+    private void create()
+    {
+        // TIMER AND BUTTON
+        this.actionableList.add(new Timer(
+                (TextView) findViewById(R.id.timer),
+                (ImageView) findViewById(R.id.buttonImage),
+                (Button) findViewById(R.id.theButton)));
+    }
+
+    /*                                          *\
+        DON'T CHANGE ANYTHING BELOW THIS POINT
+    \*                                          */
+
+    final Handler handler = new Handler();
+    final int delay = 10;//getDelay(); // Delay in milliseconds
+    ActionableList actionableList;
+    Actionable[] actionableObjects;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        this.actionableList = new ActionableList();
+
+        create();
+
+        this.actionableObjects = this.actionableList.toArray();
+    }
+
+    // Runs once per frame when activity is active
+    private void update()
+    {
+        for (int i = 0; i < this.actionableObjects.length; i++)
+        {
+            if(this.actionableObjects[i] != null)
+                this.actionableObjects[i].update();
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                update();
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    // Runs upon pausing of activity
+    private void pause()
+    {
+        for (int i = 0; i < this.actionableObjects.length; i++)
+        {
+            if(this.actionableObjects[i] != null)
+                this.actionableObjects[i].pause();
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        pause();
+    }
+
+    private int getDelay()
+    {
+        float fps = (this.FRAMES_PER_SECOND > 1000) ? 1000 : this.FRAMES_PER_SECOND;
+
+        if(fps <= 0)
+            fps = Float.MIN_NORMAL;
+
+        int delay = (int) (1000 / fps);
+
+        return delay;
+    }
+}
---
+++ app/src/main/java/com/example/timesink/Timer.java
@@ -0,0 +1,145 @@
+package com.example.timesink;
+
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Timer implements Actionable
+{
+    long startTime;
+    boolean timerRunning;
+    long totalTime;
+
+    TextView timerText;
+    ImageView buttonImage;
+    Button theButton;
+
+    public Timer(TextView timerText, ImageView buttonImage, Button theButton)
+    {
+        this.timerText = timerText;
+        this.buttonImage = buttonImage;
+        this.theButton = theButton;
+
+        this.theButton.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                // start
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                    startTimer();
+
+                    // stop
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                    stopTimer();
+
+                return true;
+            }
+        });
+
+        resetTimer();
+    }
+
+    @Override
+    public void update()
+    {
+        updateTimer();
+    }
+
+    @Override
+    public void pause()
+    {
+        stopTimer();
+    }
+
+    private long getCurrentTime()
+    { return SystemClock.elapsedRealtime(); }
+
+    private void startTimer()
+    {
+        resetTimer();
+        this.startTime = getCurrentTime();
+        this.timerRunning = true;
+        this.buttonImage.setImageResource(R.drawable.button_pressed_background);
+    }
+
+    private void updateTimer()
+    {
+        // sets the time to the current time in milliseconds
+        if(this.timerRunning)
+            setTimer(getCurrentTime() - this.startTime);
+    }
+
+    private void stopTimer()
+    {
+        this.totalTime = getCurrentTime() - this.startTime;
+        this.timerRunning = false;
+        this.buttonImage.setImageResource(R.drawable.button_unpressed_background);
+    }
+
+    private void setTimer(long millis)
+    {
+        int seconds = (int) (millis / 1000);
+        int milliseconds = (int) (millis % 1000);
+
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        int hours = minutes / 60;
+        minutes %= 60;
+
+        int days = hours / 24;
+        hours %= 24;
+
+        String timeString = formatTime(days, hours, minutes, seconds, milliseconds);
+
+        this.timerText.setText(timeString);
+    }
+
+    private String formatTime(int days, int hours, int minutes, int seconds, int milliseconds)
+    {
+        StringBuilder timeBuilder = new StringBuilder();
+
+        if(minutes > 0)
+        {
+            if(hours > 0)
+            {
+                if(days > 0)
+                {
+                    timeBuilder.append(days);
+                    timeBuilder.append(":");
+                    if(hours < 10)
+                        timeBuilder.append("0");
+                }
+
+                timeBuilder.append(hours);
+                timeBuilder.append(":");
+                if(minutes < 10)
+                    timeBuilder.append("0");
+            }
+
+            timeBuilder.append(minutes);
+            timeBuilder.append(":");
+            if(seconds < 10)
+                timeBuilder.append("0");
+        }
+
+        timeBuilder.append(seconds);
+        timeBuilder.append(".");
+
+        int hundredths = (int) (milliseconds / 10);
+        if(hundredths < 10)
+            timeBuilder.append("0");
+        timeBuilder.append(hundredths);
+
+        return timeBuilder.toString();
+    }
+
+    private void resetTimer()
+    {
+        this.timerText.setText("0.00");
+    }
+}
