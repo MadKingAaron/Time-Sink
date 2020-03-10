f4708d83044f56861c7daddd8e4566f265cec01b
Jack Koscheka <koschekajack836@gmail.com>
2020-03-09 21:18:22 -0800

    Timer Updates
    updated utilization of timer and have it update most recent score to leaderboard on release of button

--- app/src/main/java/com/example/timesink/MainActivity.java
+++ app/src/main/java/com/example/timesink/MainActivity.java
@@ -10,25 +10,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
     final float FRAMES_PER_SECOND = 100; // MAX 1000 (INCLUSIVE), MIN 0 (EXCLUSIVE) FRAMES_PER_SECOND // 0 < FRAMES_PER_SECOND <= 1000
 
     // ADD ALL OBJECTS HERE
     private void create()
     {
         // TIMER AND BUTTON
-        this.actionableList.add(new Timer(
+        SwitchTimer timer = new SwitchTimer(this, LeaderboardActivity.class,
                 (TextView) findViewById(R.id.timer),
                 (ImageView) findViewById(R.id.buttonImage),
-                (Button) findViewById(R.id.theButton)));
+                (Button) findViewById(R.id.theButton));
+
+        this.actionableList.add(timer);
     }
 
-    /*                                          *\
-        DON'T CHANGE ANYTHING BELOW THIS POINT
-    \*                                          */
+    /*                                                      *\
+        SHOULDN'T NEED TO CHANGE ANYTHING BELOW THIS POINT
+    \*                                                      */
 
     final Handler handler = new Handler();
-    final int delay = 10;//getDelay(); // Delay in milliseconds
+    final int delay = getDelay(); // Delay in milliseconds
     ActionableList actionableList;
     Actionable[] actionableObjects;
 
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity
     {
         for (int i = 0; i < this.actionableObjects.length; i++)
         {
-            if(this.actionableObjects[i] != null)
+            if (this.actionableObjects[i] != null)
                 this.actionableObjects[i].update();
         }
     }
@@ -62,8 +63,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onResume();
 
-        handler.postDelayed(new Runnable() {
-            public void run() {
+        handler.postDelayed(new Runnable()
+        {
+            public void run()
+            {
                 update();
                 handler.postDelayed(this, delay);
             }
@@ -75,7 +78,7 @@ public class MainActivity extends AppCompatActivity
     {
         for (int i = 0; i < this.actionableObjects.length; i++)
         {
-            if(this.actionableObjects[i] != null)
+            if (this.actionableObjects[i] != null)
                 this.actionableObjects[i].pause();
         }
     }
@@ -92,7 +95,7 @@ public class MainActivity extends AppCompatActivity
     {
         float fps = (this.FRAMES_PER_SECOND > 1000) ? 1000 : this.FRAMES_PER_SECOND;
 
-        if(fps <= 0)
+        if (fps <= 0)
             fps = Float.MIN_NORMAL;
 
         int delay = (int) (1000 / fps);
