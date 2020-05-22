a58584a69a5d1d15f3d6c298580bd17bdfc7cdb4
Nicholas Caballero <ncaballero9001@gmail.com>
2020-05-21 23:17:07 -0700

    Verification

--- app/src/main/java/com/example/projecttimesink/AchievementMessageVerification.java
+++ app/src/main/java/com/example/projecttimesink/AchievementMessageVerification.java
@@ -48,27 +48,42 @@ public class AchievementMessageVerification
 
     private void checkFourth()
     {
-
+        if(this.currentTime>=(oneMinute*10))
+        {
+            numberOfTimeBasedAchievementsUnlocked=4;
+        }
     }
 
     private void checkFifth()
     {
-
+        if(this.currentTime>=(oneMinute*30))
+        {
+            numberOfTimeBasedAchievementsUnlocked=5;
+        }
     }
 
     private void checkSixth()
     {
-
+        if(this.currentTime>=(oneMinute*60))
+        {
+            numberOfTimeBasedAchievementsUnlocked=6;
+        }
     }
 
     private void checkSeventh()
     {
-
+        if(this.currentTime>=(oneMinute*(60*12)))
+        {
+            numberOfTimeBasedAchievementsUnlocked=7;
+        }
     }
 
     private void checkEight()
     {
-
+        if(this.currentTime>=(oneMinute*(60*24)))
+        {
+            numberOfTimeBasedAchievementsUnlocked=8;
+        }
     }
 
     public int getNumberOfTimeBasedAchievementsUnlocked()
